c5603120b3e59e36b275fa1a32de8106cef59a6d
Claudio <claudiosilva.cfls@gmail.com>
2018-03-08 23:41:47 -0300

    Remove code with issues

--- Player.py
+++ Player.py
@@ -4,32 +4,10 @@ import math
 
 # [TODO] descobrir como alterar o group.draw pra renderizar duas imagens
 # alternativa: unir as duas imagens e passar somente a união delas
-def draw_rect(rect, screen):
-    pygame.draw.line(screen, (255,0,0), rect.topleft, rect.topright, 1)
-    pygame.draw.line(screen, (255,0,0), rect.bottomleft, rect.bottomright, 1)
-    pygame.draw.line(screen, (255,0,0), rect.topleft, rect.bottomleft, 1)
-    pygame.draw.line(screen, (255,0,0), rect.topright, rect.bottomright, 1)
-
-def create_segments(list_points, pos_back):
-    segment_list = []
-    for points in list_points:
-        points.append(points[0])
-        for i in range(0, len(points)-1):
-            screen_pos_1 = pygame.math.Vector2(pos_back) + pygame.math.Vector2(points[i])
-            screen_pos_2 = pygame.math.Vector2(pos_back) + pygame.math.Vector2(points[i+1])
-            if screen_pos_1[0] > 700 or screen_pos_1[0] < 100 or screen_pos_1[1] > 500 or screen_pos_1[1] < 100 or screen_pos_2[0] > 700 or screen_pos_2[0] < 100 or screen_pos_2[1] > 500 or screen_pos_2[1] < 100:
-                pass
-            else:
-                new_segment = {"a":{"x":screen_pos_1[0],"y":screen_pos_1[1]}, "b":{"x":screen_pos_2[0],"y":screen_pos_2[1]}}
-                segment_list.append(new_segment)
-
-    segment_list.append({"a":{"x":800,"y":0}, "b":{"x":0,"y":0}})
-    segment_list.append({"a":{"x":0,"y":600}, "b":{"x":0,"y":0}})
-    segment_list.append({"a":{"x":800,"y":600}, "b":{"x":800,"y":0}})
-    segment_list.append({"a":{"x":800,"y":600}, "b":{"x":0,"y":600}})
-    return segment_list
 
 
+# esse método de colisão tá bugado, mas apresenta desempenho minimamente razoável
+# continuarei pensando em como consertar essa parte
 def get_normal(position_on_scenario, back_mask, back_rect):
     qtd = 60
     vetor_base = pygame.math.Vector2( (35,0) )
@@ -81,103 +59,6 @@ def get_normal(position_on_scenario, back_mask, back_rect):
 
     return lista_de_angulos
 
-def draw_rays(screen, position_on_scenario, back_mask, back_rect):
-    qtd = 60
-    vetor_base = pygame.math.Vector2( (35,0) )
-    angulo = 0
-    lista_de_angulos = []
-    angulo_inicial = None
-    angulo_final = None
-
-    position_on_screen = back_rect.center + position_on_scenario
-    ha_zero = None
-    ha_final = None
-    for i in range(0,qtd):
-        vetor_rotacionado = pygame.math.Vector2(position_on_screen) + vetor_base.rotate(angulo)
-        # converte  a posição da tela para a posição no campo
-        pos_scenario =  -pygame.math.Vector2(back_rect.topleft) + vetor_rotacionado
-        x = int(pos_scenario[0])
-        y = int(pos_scenario[1])
-
-        if back_mask.get_at((x,y)):
-            cor = (255,0,0)
-            if angulo_inicial == None:
-                angulo_inicial = angulo
-            else:
-                angulo_final = angulo
-        else:
-            cor = (0,255,0)
-            if angulo_final != None:
-                if angulo_inicial == 0:
-                    ha_zero = angulo_final
-                elif angulo_final == 354:
-                    ha_final = angulo_inicial
-                else:
-                    diff = (angulo_final + angulo_inicial)/2
-                    lista_de_angulos.append( diff )
-                
-                angulo_final = None
-                angulo_inicial = None
-        pygame.draw.line(screen, cor, position_on_screen, vetor_rotacionado, 2)
-        angulo += 360/qtd
-
-    if angulo_inicial != None and angulo_final != None:
-        diff = (angulo_final, angulo_inicial)
-        lista_de_angulos.append( diff )
-
-    if ha_zero != None and ha_final != None:
-        diff = (ha_zero + ha_final)/2 - 180
-        lista_de_angulos.append( diff )
-
-    return lista_de_angulos 
-
-
-def draw_vision(screen, position_on_scenario, back_mask, back_rect):
-    qtd = 60
-    vetor_base_original = pygame.math.Vector2( (1,0) )
-    angulo = 0
-
-    position_on_screen = back_rect.center + position_on_scenario
-
-    for i in range(0,qtd):
-        # print(angulo)
-        
-
-        # pygame.draw.line(screen, (0,255,255), position_on_screen, vetor_rotacionado_original*50, 2)
-        # converte  a posição da tela para a posição no campo
-        mult = 1
-        bateu = False
-
-        while not bateu: 
-            vetor_base = vetor_base_original*mult
-            vetor_rotacionado = pygame.math.Vector2(position_on_screen) + vetor_base.rotate(angulo)
-            # vetor_rotacionado = mult*vetor_rotacionado_original
-            # if vetor_rotacionado[0] > 800 or vetor_rotacionado[0] < 0 or vetor_rotacionado[1] > 600 or vetor_rotacionado[1] < 0:
-            #     pygame.draw.line(screen, (0,255,0), position_on_screen, vetor_rotacionado, 2)
-            #     print('bateu 1')
-            #     bateu = True
-            # else:
-
-            # print(vetor_rotacionado)
-
-            pos_scenario =  -pygame.math.Vector2(back_rect.topleft) + vetor_rotacionado
-            x = int(pos_scenario[0])
-            y = int(pos_scenario[1])
-            try:
-                ponto = back_mask.get_at((x,y))
-                if ponto:
-                    pygame.draw.line(screen, (0,255,0), position_on_screen, vetor_rotacionado, 2)
-                    bateu = True
-                    # print('bateu 2')
-                else:
-                    mult += 1
-            except:
-                pygame.draw.line(screen, (0,255,0), position_on_screen, vetor_rotacionado, 2)
-                bateu = True
-                # print('bateu 3')
-        angulo += 360/qtd
-   
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, image, back_image, location_on_scenario, location_on_screen, animation, sound, background):
         super().__init__()
@@ -200,17 +81,13 @@ class Player(pygame.sprite.Sprite):
         self.position_on_screen = pygame.math.Vector2(location_on_screen)
         self.position_on_scenario = pygame.math.Vector2(location_on_scenario)
 
-        self.is_colliding = False
         # handle events
+        self.is_colliding = False
         self.pressionou_w = False
         self.pressionou_a = False
         self.pressionou_s = False
         self.pressionou_d = False
         self.is_shooting = False
-
-
-        # teste
-        self.vision = SightAndLight(self.position_on_screen)
 
     def update(self):
         self.react_to_event()
@@ -219,23 +96,10 @@ class Player(pygame.sprite.Sprite):
     # esse metodo não sobrescreveu
     # a draw é um metodo do grupo
     def draw(self, screen):
+        screen.blit(self.feet, self.feet.get_rect(center=self.position_on_screen).topleft)
         screen.blit(self.image, self.rect)
-        screen.blit(self.image, (0,0))
-        # screen.blit(self.feet, self.position_on_screen)
-        # screen.blit(self.back_image, self.rect_back)
-        olist = self.mask.outline()
-        pygame.draw.lines(screen,(200,150,150), 1, olist)
-        if self.pressionou_w or self.pressionou_d or self.pressionou_a or self.pressionou_s:
-            new_segments = create_segments(self.background.list_points, self.background.rect.topleft)
-            self.vision.run(screen, new_segments, self.angle_param)
 
-        # draw_rect(self.rect, screen)
-        # draw_rays(screen, self.position_on_scenario, self.background.mask, self.background.rect)
-        
-        # draw_vision(screen, self.position_on_scenario, self.background.mask, self.background.rect)
-            
     def move(self, direction):
-
         if not self.is_possible_direction(direction):
             angles = get_normal(self.temp_position_on_scenario, self.background.mask, self.background.rect)
             if len(angles) <= 1:
@@ -261,7 +125,6 @@ class Player(pygame.sprite.Sprite):
                     self.index_animation_move += 1
                     if self.index_animation_move == len(self.animation.move):
                         self.index_animation_move = 0
-
         else:
             try:
                 self.position_on_scenario += 5*(direction/(direction.length()))
@@ -380,146 +243,3 @@ class Player(pygame.sprite.Sprite):
 
         self.feet = self.animation.run[self.index_animation_move]
         self.original_feet = self.animation.run[self.index_animation_move]
-
-
-
-##########################################################################################################
-class SightAndLight():
-    def __init__(self, location_on_screen):
-        # segments
-        self.mouse_pos = location_on_screen
-        
-        self.segments = []
-        self.angle_param = None
-        # intersects
-        self.intersects = []
-        self.screen = None
-        # Points
-        self.points = []
-
-    def run(self, screen, new_segments, angle_param):
-        self.screen = screen
-        self.angle_param = angle_param
-        self.segments = new_segments
-        self.update()
-        self.render_frame()
-
-    def update(self):
-        # Clear old points
-        self.points = []
-
-        # Get all unique points
-        for segment in self.segments:
-            self.points.append((segment['a'], segment['b']))
-
-        unique_points = []
-        for point in self.points:
-            if point not in unique_points:
-                unique_points.append(point)
-
-        # Get all angles
-        unique_angles = []
-        for point in unique_points:
-            angle = math.atan2(point[0]["y"]-self.mouse_pos[1], point[0]["x"]-self.mouse_pos[0])
-            print(math.degrees(angle) )
-            if math.degrees(angle) < self.angle_param -90 + 50 or math.degrees(angle) > self.angle_param -90 - 50:
-                point[0]["angle"] = angle
-                unique_angles.append(angle-0.00001)
-                unique_angles.append(angle)
-                unique_angles.append(angle+0.00001)
-
-        # RAYS IN ALL DIRECTIONS
-        self.intersects = []
-        for angle in unique_angles:
-            # Calculate dx & dy from angle
-            dx = math.cos(angle)
-            dy = math.sin(angle)
-
-            # Ray from center of screen to mouse
-            ray = {
-                    "a": {"x":self.mouse_pos[0], "y": self.mouse_pos[1]},
-                    "b": {"x": self.mouse_pos[0]+dx, "y": self.mouse_pos[1]+dy}
-            }
-
-            # Find CLOSEST intersection
-            closest_intersect = None
-            for segment in self.segments:
-                intersect = self.get_intersection(ray, segment)
-                if not intersect: continue
-                if not closest_intersect or intersect["param"] < closest_intersect["param"]:
-                    closest_intersect = intersect
-
-            # Intersect angle
-            if not closest_intersect: continue
-            closest_intersect["angle"] = angle
-
-            # Add to list of intersects
-            self.intersects.append(closest_intersect)
-
-        # Sort intersects by angle
-        self.intersects = sorted(self.intersects, key=lambda k: k['angle'])
-
-    def render_frame(self):
-
-        # draw segments
-        # for segment in self.segments:
-        #     pygame.draw.aaline(self.screen, (153, 153, 153), (segment['a']['x'], segment['a']['y']), (segment['b']['x'], segment['b']['y']))
-
-        self.draw_polygon(self.intersects, (221, 56, 56))
-
-        # draw debug lines
-        for intersect in self.intersects:
-            pygame.draw.aaline(self.screen, (255, 85, 85), self.mouse_pos, (intersect['x'], intersect['y']))
-
-    def get_intersection(self, ray, segment):
-        ''' Find intersection of RAY & SEGMENT '''
-        # RAY in parametric: Point + Direction*T1
-        r_px = ray['a']['x']
-        r_py = ray['a']['y']
-        r_dx = ray['b']['x'] - ray['a']['x']
-        r_dy = ray['b']['y'] - ray['a']['y']
-
-        # SEGMENT in parametric: Point + Direction*T2
-        s_px = segment['a']['x']
-        s_py = segment['a']['y']
-        s_dx = segment['b']['x'] - segment['a']['x']
-        s_dy = segment['b']['y'] - segment['a']['y']
-
-        # Are they parallel? If so, no intersect
-        r_mag = math.sqrt(r_dx*r_dx+r_dy*r_dy)
-        s_mag = math.sqrt(s_dx*s_dx+s_dy*s_dy)
-        if r_mag!=0 and s_mag!=0:
-            if r_dx/r_mag == s_dx/s_mag and r_dy/r_mag == s_dy/s_mag:
-                return None
-        else:
-            return None
-        try:
-            T2 = (r_dx*(s_py-r_py) + r_dy*(r_px-s_px))/(s_dx*r_dy - s_dy*r_dx)
-        except:
-            # T2 = (r_dx*(s_py-r_py) + r_dy*(r_px-s_px))/(s_dx*r_dy - s_dy*r_dx-0.01)
-            T2 = 32000000
-
-        try:
-            T1 = (s_px+s_dx*T2-r_px)/r_dx
-        except:
-            # T1 = (s_px+s_dx*T2-r_px)/(r_dx-0.01)
-            T1 = 32000000
-
-        if T1 < 0: return None
-        if T2 < 0 or T2>1: return None
-
-        return {
-                "x": r_px+r_dx*T1,
-                "y": r_py+r_dy*T1,
-                "param": T1
-        }
-
-    def draw_polygon(self, polygon, color):
-        # collect coordinates for a giant polygon
-        points = []
-        for intersect in polygon:
-            points.append((intersect['x'], intersect['y']))
-        
-        # draw as a giant polygon
-        pygame.gfxdraw.aapolygon(self.screen, points, color)
-        pygame.gfxdraw.filled_polygon(self.screen, points, color)

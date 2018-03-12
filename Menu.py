c73e6459e91148d4a112f80719eceb958abde118
EricToshio <eric_toshio@hotmail.com>
2018-03-12 01:23:59 -0300

    multiplayer menu input name almost done

--- Menu.py
+++ Menu.py
@@ -3,9 +3,16 @@ import time
 from NetworkSettings import *
 import NetworkServer
 from pyshooterClient import *
+from textbox import TextBox
+
+KEY_REPEAT_SETTING = (200,70)
+
 
 class Menu():
     def __init__(self):
+        self.name = "McLoma"
+        self.screen = pg.display.get_surface()
+        self.color = (100, 100, 100)
         self._in_menu = True
         self._in_menu_multiplayer = False
         self.MENU_IMAGE = pg.image.load("Assets/Images/menu/background.png")
@@ -36,9 +43,9 @@ class Menu():
         if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
             self._in_menu = False
             self._in_menu_multiplayer = False
-        if event.type == pg.KEYDOWN:
-            self._in_menu = False
-            self._in_menu_multiplayer = False
+        # if event.type == pg.KEYDOWN:
+        #     self._in_menu = False
+        #     self._in_menu_multiplayer = False
 
     def interactive(self, event):
         mouse = pg.mouse.get_pos()
@@ -65,7 +72,7 @@ class Menu():
         if 150 + 200 > mouse[0] > 150 and 450 + 50 > mouse[1] > 450:
             self.surface.blit(self.SERVER_ON, (150, 450))
             if event.type == pg.MOUSEBUTTONDOWN:
-                self.server_client = pyshooterClient('Name')
+                self.server_client = pyshooterClient(self.name)
                 self.server_client.start()
                 self.have_client = True
                 self._in_menu_multiplayer = False
@@ -78,6 +85,19 @@ class Menu():
                 self._in_menu_multiplayer = False
         else:
             self.surface.blit(self.CONNECT_OFF, (450, 450))
+
+    def change_name(self,id,name):
+        try:
+            self.name = str(name)
+        except ValueError:
+            print("Please input a valid color name.")
+
+    def make_prompt(self):
+        font = pg.font.SysFont("arial", 20)
+        message = 'Type your name :'
+        rend = font.render(message + self.name, True, pg.Color("white"))
+        return (rend, rend.get_rect(topleft=(10, 35)))
+
 
     def intro(self):
         self.surface = pg.display.get_surface()
@@ -98,10 +118,20 @@ class Menu():
             for event in pg.event.get():
                 if event.type == pg.MOUSEBUTTONUP:
                     button_up = False
-
+        self.input = TextBox((100, 100, 150, 30), command=self.change_name,
+                             clear_on_enter=True, inactive_on_enter=False)
+        self.prompt = self.make_prompt()
+        pg.key.set_repeat(*KEY_REPEAT_SETTING)
         while(self._in_menu_multiplayer):
             for event in pg.event.get():
+                self.surface.blit(self.MENU_IMAGE, (0, 0))
                 self.on_event(event)
                 self.interactive_multiplayer(event)
+                self.input.get_event(event)
+                self.input.update()
+                self.input.draw(self.surface)
+                self.prompt = self.make_prompt()
+
+                self.surface.blit(*self.prompt)
             self.on_render()
         self.music.stop()

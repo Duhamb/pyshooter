43de0fd108ef22266f969dda73996e9faa8344c0
EricToshio <eric.toshio.e.s@gmail.com>
2018-03-12 10:14:49 -0300

    improve light fps and remove venv

--- Light.py
+++ Light.py
@@ -7,8 +7,10 @@ class Light():
 		self.light_rect = self.light_mask.get_rect()
 		self.player = player
 		self.color = (5,5,5)
-	def draw(self, screen):
 		self.fog.fill(self.color)
 		self.light_rect.center = self.player.position_on_screen
 		self.fog.blit(self.light_mask, self.light_rect)
+
+	def draw(self, screen):
+
 		screen.blit(self.fog, (0, 0), special_flags=pg.BLEND_MULT)
